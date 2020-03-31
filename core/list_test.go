faf4ed5646a0dbf3102ef5de116427fcfc4bbf96
Gabriel Totev <gttotev8@gmail.com>
2020-03-30 19:57:08 -0700

    Add negative unit test

--- core/list_test.go
+++ core/list_test.go
@@ -1,11 +1,29 @@
 package core
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func TestListBuckets(t *testing.T) {
 	if _, err := ListBuckets(); err != nil {
 		t.Error(err)
 	}
+}
+
+func TestListBucketsError(t *testing.T) {
+	tempS3 := s3Svc
+	s3Svc = s3.New(session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials("not", "a", "thing"),
+	})))
+	if res, err := ListBuckets(); err == nil {
+		fmt.Println(res)
+		t.Error("Bad credentials! Should fail!")
+	}
+	s3Svc = tempS3
 }
